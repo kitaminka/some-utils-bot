756ca28c126f4d993a382b4998ceadebe2f6f0a1
Kitaminka <kit4minka@gmail.com>
2021-02-14 20:37:01 +0200

    Add env file

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const client = new Discord.Client();
 require('./handlers/commands')(client);
 require('./handlers/events')(client);
 
-client.login('ODEwNTc2NzE5OTAzNzE5NDI0.YClqZQ.JbwFN6qWb2ArSlu8k4Z4jSfRUbs');
+client.login(process.env.TOKEN);
