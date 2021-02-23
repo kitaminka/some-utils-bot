4d1ff4d2d79dbb52001e4168683383b360a7bad5
Kitaminka <kit4minka@gmail.com>
2021-02-23 21:24:59 +0200

    Update info command

--- commands/information/info.js
+++ commands/information/info.js
@@ -8,7 +8,7 @@ module.exports = {
             .setTitle('SomeUtilsBot information')
             .setColor(client.config.embedColor)
             .setDescription(`It is bot with a set of utilities.\nCurrent server bot prefix: \`${message.prefix}\`. Use \`${message.prefix}help\` to see all bot command categories.`)
-            .addField('Links', '[**Add bot to server**](https://discord.com/oauth2/authorize?client_id=810576719903719424&scope=bot&permissions=1849163457)\n[**Support server**](https://discord.gg/fdW283BpTY)\n[**GitHub repository**](https://github.com/Kitaminka/SomeUtilsBot)')
+            .addField('Links', '[Add bot to server](https://discord.com/oauth2/authorize?client_id=810576719903719424&scope=bot&permissions=1849163457)\n[Support server](https://discord.gg/fdW283BpTY)\n[GitHub repository](https://github.com/Kitaminka/SomeUtilsBot)')
             .setTimestamp();
 
         return message.channel.send(embed);
