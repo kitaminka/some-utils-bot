ff7a098cede63580872172fc6431de8e51996c3f
Kitaminka <kit4minka@gmail.com>
2021-02-28 22:49:05 +0200

    Fixed incorrect prefix in help command

--- commands/information/help.js
+++ commands/information/help.js
@@ -7,7 +7,7 @@ module.exports = {
         let embed = new Discord.MessageEmbed();
         if (!args[0]) {
             embed.setTitle(':dividers:Command categories')
-                .setDescription('To know more about category or command, use `!help [category or command name]`.')
+                .setDescription(`To know more about category or command, use \`${message.prefix}help [category or command name]\`.`)
                 .setColor(client.config.embedColor)
                 .setTimestamp();
             for (const category of client.commandCategories) {
@@ -23,7 +23,7 @@ module.exports = {
             args[0] = args[0].toLowerCase();
             if (client.commandCategories.includes(args[0])) {
                 embed.setTitle(`:dividers:${args[0].charAt(0).toUpperCase() + args[0].slice(1)} commands`)
-                    .setDescription('To know more about category or command, use `!help [category or command name]`.')
+                    .setDescription(`To know more about category or command, use \`${message.prefix}help [category or command name]\`.`)
                     .setColor(client.config.embedColor)
                     .setTimestamp();
                 let commands = [];
@@ -39,13 +39,13 @@ module.exports = {
             } else if (client.commands.has(args[0])) {
                 const commandInfo = client.commands.get(args[0])
                 embed.setTitle(`:dividers:Command ${message.prefix + args[0]}`)
-                    .setDescription('To know more about category or command, use `!help [category or command name]`.')
+                    .setDescription(`To know more about category or command, use \`${message.prefix}help [category or command name]\`.`)
                     .setColor(client.config.embedColor)
                     .setTimestamp()
                     .addField('Category', commandInfo.category.charAt(0).toUpperCase() + commandInfo.category.slice(1))
                     .addField('Description', commandInfo.description);
             } else {
-                embed = await client.modules.errorEmbed(client, 'Entered incorrect category or command name. Use `!help` to get all command categories.');
+                embed = await client.modules.errorEmbed(client, `Entered incorrect category or command name. Use \`${message.prefix}help\` to get all command categories.`);
             }
         }
         return message.channel.send(embed);
