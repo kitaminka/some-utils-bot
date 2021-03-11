7abf8936244132f107a5a1fc76050b38629a1f0e
isimsizz94 <58332630+isimsizz94@users.noreply.github.com>
2021-03-11 14:21:30 +0500

    а

--- commands/other/embed.js
+++ commands/other/embed.js
@@ -13,16 +13,16 @@ module.exports = {
             return message.channel.send(await client.modules.errorEmbed(client, 'Failed to get embed information.'));
         }
         const embed = new Discord.MessageEmbed();
-        if (embedInfo.title !== undefined) embed.setTitle(embedInfo.title);
-        if (embedInfo.url !== undefined) embed.setURL(embedInfo.url);
-        if (embedInfo.author.name !== undefined && embedInfo.author.image !== undefined && embedInfo.author.url !== undefined) embed.setAuthor(embedInfo.author.name, embedInfo.author.image, embedInfo.author.url);
-        if (embedInfo.description !== undefined) embed.setDescription(embedInfo.description);
-        if (embedInfo.fields !== undefined) embed.addFields(embedInfo.fields);
-        if (embedInfo.image !== undefined) embed.setImage(embedInfo.image);
-        if (embedInfo.thumbnail !== undefined) embed.setThumbnail(embedInfo.thumbnail);
-        if (embedInfo.color !== undefined) embed.setColor(embedInfo.color);
-        if (embedInfo.footer.text !== undefined && embedInfo.footer.image !== undefined) embed.setFooter(embedInfo.footer.text, embedInfo.footer.image);
-        if (embedInfo.timestamp !== undefined) embed.setTimestamp(embedInfo.timestamp);
+        if (embedInfo.title) embed.setTitle(embedInfo.title);
+        if (embedInfo.url) embed.setURL(embedInfo.url);
+        if (embedInfo.author.name && embedInfo.author.image && embedInfo.author.url) embed.setAuthor(embedInfo.author.name, embedInfo.author.image, embedInfo.author.url);
+        if (embedInfo.description) embed.setDescription(embedInfo.description);
+        if (embedInfo.fields) embed.addFields(embedInfo.fields);
+        if (embedInfo.image) embed.setImage(embedInfo.image);
+        if (embedInfo.thumbnail) embed.setThumbnail(embedInfo.thumbnail);
+        if (embedInfo.color) embed.setColor(embedInfo.color);
+        if (embedInfo.footer.text && embedInfo.footer.image) embed.setFooter(embedInfo.footer.text, embedInfo.footer.image);
+        if (embedInfo.timestamp) embed.setTimestamp(embedInfo.timestamp);
 
         return message.channel.send(embed);
     }
