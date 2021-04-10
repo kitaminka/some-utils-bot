d4d29f3965b67b1c0224c96fed19d43f56891b6a
Nikita <kit4minka@gmail.com>
2021-04-10 21:07:24 +0300

    Update steammarket command

--- commands/other/steammarket.js
+++ commands/other/steammarket.js
@@ -10,11 +10,14 @@ module.exports = {
         // TODO Add catch for requests
         if (!args[0]) return message.channel.send(await client.modules.errorEmbed(client, 'No Steam Community Market item name specified.'));
         const itemInfo = await fetch(`https://steamcommunity.com/market/search/render/?search_descriptions=0&sort_column=default&sort_dir=desc&norender=1&count=1&query=${args.join(' ')}`, {
-            method: 'GET'
+            method: 'GET',
+            headers: {
+                cookie: process.env.STEAM_LOGIN
+            }
         }).then(res => res.json());
         if (!itemInfo.success) return message.channel.send(await client.modules.errorEmbed(client, 'Failed to get Community Market item information.'));
         if (itemInfo.results === []) return message.channel.send(await client.modules.errorEmbed(client, 'Failed to find Community Market item.'));
-        const priceInfo = await fetch(`https://steamcommunity.com/market/pricehistory/?currency=1&appid=730&market_hash_name=${itemInfo.results[0].hash_name}`, {
+        const priceInfo = await fetch(`https://steamcommunity.com/market/pricehistory/?country=GB&currency=1&appid=${itemInfo.results[0].asset_description.appid}&market_hash_name=${itemInfo.results[0].hash_name}`, {
             method: 'GET',
             headers: {
                 cookie: process.env.STEAM_LOGIN
@@ -23,10 +26,10 @@ module.exports = {
         if (!priceInfo.success) return message.channel.send(await client.modules.errorEmbed(client, 'Failed to get Community Market item information.'));
         const dates = [],
             price = [];
-        for (const info of priceInfo.prices) {
-            if (dates.length === 0 || new Date(info[0]).getUTCDate() !== dates[dates.length - 1].getUTCDate()) {
-                dates.push(new Date(info[0]));
-                price.push(info[1]);
+        for (let i = priceInfo.prices.length - 1; i > 0; i--) {
+            if (dates.length === 0 || (new Date(priceInfo.prices[i][0]).getUTCDate() !== dates[dates.length - 1].getUTCDate() && dates.length < 100)) {
+                dates.push(new Date(priceInfo.prices[i][0]));
+                price.push(priceInfo.prices[i][1]);
             }
         }
         const chart = new QuickChart().setConfig({
@@ -34,7 +37,6 @@ module.exports = {
                 data: {
                     labels: dates,
                     datasets: [{
-                        label: 'Price',
                         data: price,
                         fill: false,
                         borderColor: client.config.embedColor,
@@ -66,14 +68,26 @@ module.exports = {
                                 color: '#FFFFFF'
                             }
                         }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: 'Price',
+                        fontSize: 20,
+                        fontColor: '#FFFFFF'
                     }
                 }
             }).setBackgroundColor('#2C2F33');
         const embed = new Discord.MessageEmbed()
-            .setTitle(args.join(' '))
+            .setTitle(itemInfo.results[0].name)
+            .setAuthor(itemInfo.results[0].app_name, itemInfo.results[0].app_icon)
             .setColor(client.config.embedColor)
+            .setThumbnail(`https://community.cloudflare.steamstatic.com/economy/image/${itemInfo.results[0].asset_description.icon_url}`)
+            .addField('Price', itemInfo.results[0].sell_price_text)
+            .addField('Type', itemInfo.results[0].asset_description.type)
             .setImage(await chart.getShortUrl());
-        if (message.deletable) await message.delete();
         return message.channel.send(embed);
     }
 }
