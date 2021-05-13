9675705a0e87033380d53761d15e0ddb3a692c02
Nikita <kit4minka@gmail.com>
2021-05-13 17:52:35 +0300

    Remove ping command, WebSocket ping add to info command

--- commands/information/info.js
+++ commands/information/info.js
@@ -17,8 +17,9 @@ module.exports = {
             .setDescription(`It is bot with a set of utilities.\nCurrent server bot prefix: \`${message.prefix}\`. Use \`${message.prefix}help\` to see all bot command categories.`)
             .addField('Uptime', `${days}d ${hours}h ${minutes}m ${seconds}s`)
             .addField('Server count', client.guilds.cache.size)
-            .addField('Developer', 'Kitam#5350')
+            .addField('Developer', 'Kitam#5450')
             .addField('Node.js version', process.version)
+            .addField('WebSocket ping', client.ws.ping)
             .addField('Links', '[Add bot to server](https://discord.com/oauth2/authorize?client_id=810576719903719424&scope=bot&permissions=1849163457)\n[Support server](https://discord.gg/fdW283BpTY)\n[GitHub repository](https://github.com/Kitaminka/SomeUtilsBot)')
             .setTimestamp();
 
