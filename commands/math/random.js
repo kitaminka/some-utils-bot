6ad648148dd65bc53785ba60cd00d106f9f088bf
Nikita <kit4minka@gmail.com>
2021-05-14 15:22:46 +0300

    Fixed random command

--- commands/math/random.js
+++ commands/math/random.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 module.exports = {
     name: 'random',
@@ -8,7 +8,7 @@ module.exports = {
         if (!args[0] || !args[1]) return message.channel.send(await client.modules.errorEmbed(client, 'The smallest or biggest number is not specified.'));
         if (isNaN(args[0]) || isNaN(args[1]) || args[0].includes('.') || args[1].includes('.')) return message.channel.send(await client.modules.errorEmbed(client, 'The smallest or biggest number must be an integer.'));
         if (args[0] === args[1]) return message.channel.send(await client.modules.errorEmbed(client, 'The first number cannot be equal to the second number.'));
-        if (args[0] < args[1]) return message.channel.send(await client.modules.errorEmbed(client, 'The first number cannot be smaller than the second number.'));
+        if (args[0] > args[1]) return message.channel.send(await client.modules.errorEmbed(client, 'The first number cannot be smaller than the second number.'));
 
         const number = await fetch(`https://www.random.org/integers/?num=1&min=${args[0]}&max=${args[1]}&base=10&col=1&format=plain&rnd=new`, {
             method: 'GET'
