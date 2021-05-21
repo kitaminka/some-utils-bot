fb6da4a6780f74765a3b9cff606559c5c406dce4
Nikita <kit4minka@gmail.com>
2021-05-21 09:51:44 +0300

    Create solve command

---
+++ commands/math/solve.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'solve',
+    description: 'Solve math example.',
+    async execute(client, message, args) {
+        if (!args[0]) return message.channel.send(await client.modules.errorEmbed(client, 'The math example is not specified.'));
+        const example = args.join(' ');
+        if (/[a-z]/.test(example) || /[,'"`$@#/?]/.test(example)) return message.channel.send(await client.modules.errorEmbed(client, 'The math example is invalid.'));
+        if (example.length > 50) return message.channel.send(await client.modules.errorEmbed(client, 'The math example must be shorter than 50 symbols.'));
+        try {
+            const result = eval(example);
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`:heavy_plus_sign: Result of math example`)
+                .setColor(client.config.embedColor)
+                .addField('Math example', example)
+                .addField('Result', result)
+                .setTimestamp();
+            return message.channel.send(embed);
+        } catch {
+            return message.channel.send(await client.modules.errorEmbed(client, 'The math example is invalid.'));
+        }
+    }
+}
