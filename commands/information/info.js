0b4ddef6c9f3dd2b7764e0993ac25e0d2f47b645
Kitaminka <kit4minka@outlook.com>
2021-10-23 18:42:41 +0300

    Update info command

--- commands/information/info.js
+++ commands/information/info.js
@@ -16,7 +16,6 @@ module.exports = {
             .setDescription(`It is bot with a set of utilities.\nCurrent server bot prefix: \`${message.prefix}\`. Use \`${message.prefix}help\` to see all bot command categories.`)
             .addField('Uptime', `${days}d ${hours}h ${minutes}m ${seconds}s`)
             .addField('Server count', client.guilds.cache.size)
-            .addField('Developer', 'Kitam#5450')
             .addField('Node.js version', process.version)
             .addField('WebSocket ping', client.ws.ping)
             .addField('Links', '[GitHub repository](https://github.com/Kitaminka/SomeUtilsBot)')
